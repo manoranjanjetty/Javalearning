cac71e9c2f1d1ab46f4128f1d079f6ea980ba677
manoranjanjetty <manoranjanjetty@gmail.com>
2023-07-05 14:41:51 +0100

    latest learnings

---
+++ src/Variablefun.java
@@ -0,0 +1,13 @@
+public class Variablefun {
+    public static void main(String[] args) {
+        int age;
+        age=30;
+        String name = "Manoranjan";
+        String hometown = "Shimoga";
+        double pie=3.14;
+        float pie1=3.14f;
+        System.out.println(name + " is " + age);
+        System.out.println(hometown);
+        System.out.println(pie+pie1);
+    }
+}
